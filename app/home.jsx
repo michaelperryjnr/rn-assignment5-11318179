f4c1a7ba456edeca2d401ba9b0685e8512f9abaf
michaelperryjnr <michaelperryt97@gmail.com>
2024-06-26 20:53:44 +0000

    Finished app

--- app/home.jsx
+++ app/home.jsx
@@ -1,9 +1,77 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, SectionList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { CustomButton } from "../components";
+import { CustomButton, Transaction } from "../components";
 import { icons, images } from "../constants";
 import { useTheme } from "../components/ThemeContext";
+
+const data = [
+  {
+    id: 1,
+    name: "Apple Store",
+    amount: "-$250",
+    icon: icons.apple,
+    category: "Entertainment",
+    isPositive: false,
+  },
+  {
+    id: 2,
+    name: "Spotify",
+    amount: "-$12,99",
+    icon: icons.spotify,
+    category: "Music",
+    isPositive: false,
+  },
+  {
+    id: 3,
+    name: "Money Transfer",
+    amount: "$300",
+    icon: icons.receive,
+    category: "Transaction",
+    isPositive: true,
+  },
+  {
+    id: 4,
+    name: "Grocery",
+    amount: "-$88",
+    icon: icons.grocery,
+    category: "Food",
+    isPositive: false,
+  },
+  {
+    id: 5,
+    name: "Apple Store",
+    amount: "-$250",
+    icon: icons.apple,
+    category: "Entertainment",
+    isPositive: false,
+  },
+  {
+    id: 6,
+    name: "Spotify",
+    amount: "-$12,99",
+    icon: icons.spotify,
+    category: "Music",
+    isPositive: false,
+  },
+  {
+    id: 7,
+    name: "Money Transfer",
+    amount: "$300",
+    icon: icons.receive,
+    category: "Transaction",
+    isPositive: true,
+  },
+  {
+    id: 8,
+    name: "Grocery",
+    amount: "-$88",
+    icon: icons.grocery,
+    category: "Food",
+    isPositive: false,
+  },
+];
+
 
 export default function Home() {
   const {isDarkMode} = useTheme();
@@ -141,13 +209,20 @@ export default function Home() {
           <Text
             className={`${
               isDarkMode ? "text-black-300" : "text-black-300"
-            } font-medium text-xl`}
+            } font-medium text-xl mb-6`}
           >
             See All
           </Text>
         </View>
+        <SectionList 
+        sections={[{title: "Transaction", data: data}]}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({item}) => <Transaction name={item.name} amount={item.amount} icon={item.icon} category={item.category}
+        isPositive={item.isPositive}
+        />}
+        />
       </View>
-      <StatusBar backgroundColor="#FFFFFF" style="auto" />
+      <StatusBar backgroundColor={`${isDarkMode ? "#FFFFFF" : "#0C0C12"}`} style="auto" />
     </SafeAreaView>
   );
 }
